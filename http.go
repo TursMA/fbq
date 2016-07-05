e7f608f055095454305b2dc9e9849302e1c00703
aeud <adrien.eudes@gmail.com>
2016-07-05 11:41:55 +0800

    Log GET Urls

--- http.go
+++ http.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HttpGet(url string) *http.Response {
-	// fmt.Printf("GET %v\n", url)
+	fmt.Printf("GET %v\n", url)
 	resp, err := http.Get(url)
 	catchError(err)
 	if resp.StatusCode != 200 {
