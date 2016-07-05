88ba32a49ff881482c3d8d6847202ad66c866a94
aeud <adrien.eudes@gmail.com>
2016-07-05 11:12:31 +0800

    Un Async

--- http.go
+++ http.go
@@ -8,13 +8,13 @@ import (
 )
 
 func HttpGet(url string) *http.Response {
-	fmt.Printf("GET %v\n", url)
+	// fmt.Printf("GET %v\n", url)
 	resp, err := http.Get(url)
 	catchError(err)
 	if resp.StatusCode != 200 {
 		if resp.StatusCode == 400 || resp.StatusCode == 500 {
 			fmt.Printf("RETRY--- ERROR %v while getting %v -----\n", resp.Status, url)
-			time.Sleep(10 * time.Second)
+			time.Sleep(time.Minute)
 			return HttpGet(url)
 		} else {
 			log.Fatalf("----- ERROR %v while getting %v -----\n", resp.Status, url)
